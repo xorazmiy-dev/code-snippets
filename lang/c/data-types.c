1c2e398d854a2f60c885c67e2747d46a97c6d763
Ulfat Abjabborov <ulfat.abjabborov@gmail.com>
2023-10-02 07:35:55 +0200

    Add c/data-types.c

---
+++ lang/c/data-types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int butun_son = 18;
+    float kasr_son = 18.5;
+    char belgi = 'A';
+
+    printf("butun_son = %d\n", butun_son);
+    printf("kasr_son = %f\n", kasr_son);
+    printf("belgi = %c\n", belgi);
+}
