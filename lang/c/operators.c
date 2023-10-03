da09c92ef8b12bc1f91b676d45c1a569553c3c1d
Ulfat Abjabborov <ulfat.abjabborov@gmail.com>
2023-10-03 07:45:16 +0200

    Add operators.c

---
+++ lang/c/operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2 + 3; 
+    int b = 2 * 3;
+    int c = 2 - 3;
+    int d = 4 / 2;
+    int e = 2 + 3 * 4; 
+    int f = (2 + 3) * 4;
+    int g = 2 + 4 / 2;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("c = %d\n", c);
+    printf("d = %d\n", d);
+    printf("e = %d\n", e);
+    printf("f = %d\n", f);
+    printf("g = %d\n", g);
+}
