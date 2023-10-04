b0c121558914c0785589399663dc69db1380a4d1
Ulfat Abjabborov <ulfat.abjabborov@gmail.com>
2023-10-04 22:06:48 +0200

    Add if-else-statement.c

---
+++ lang/c/if-else-statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 9;
+
+    if (x > 10) {
+        printf("x 10 dan kattaroq\n");
+    } else {
+        printf("x 10 dan kichik yoki teng\n");
+    }
+
+    return 0;
+}
