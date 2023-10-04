2fcdb2f3112905a1bfed2bf5bbd7bc4b86eb01c9
Ulfat Abjabborov <ulfat.abjabborov@gmail.com>
2023-10-04 22:24:25 +0200

    Add if-elseif-else.c

---
+++ lang/c/if-elseif-else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+
+    if (x > 10) {
+        printf("x 10 dan katta\n");
+    } else if (x == 10) {
+        printf("x 10 ga teng\n");
+    } else {
+        printf("x 10 dan kichik yoki teng\n");
+    }
+
+    return 0;
+}
