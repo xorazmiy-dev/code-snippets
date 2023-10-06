07c04f45197769a0618d561a0a72bf3a16392668
Ulfat Abjabborov <ulfat.abjabborov@gmail.com>
2023-10-06 03:30:10 +0200

    Add loops-unwrapped.c

---
+++ lang/c/loops-unwrapped.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("%d\n", 1);
+    printf("%d\n", 2);
+    printf("%d\n", 3);
+    printf("%d\n", 4);
+    printf("%d\n", 5);
+    printf("%d\n", 6);
+    printf("%d\n", 7);
+    printf("%d\n", 8);
+    printf("%d\n", 9);
+    printf("%d\n", 10);
+
+    return 0;
+}
