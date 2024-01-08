2fa5682e570631f1183ad01fe874eef20c08d44d
Sitora Tuychieva <situychieva@gmail.com>
2024-01-09 00:50:52 +0500

    Add printf.c, scanf.c

---
+++ lang/c-beginner/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int son = 10;
+    printf("Sonning qiymati: %d\n", son);
+    return 0;
+}
---
+++ lang/c-beginner/scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int son;
+    scanf("%d", &son);
+    printf("Siz kiritgan son: %d\n", son);
+    return 0;
+}
