841f24710c15d9f654b2df47eca5b6a49407d394
Sitora Tuychieva <situychieva@gmail.com>
2024-01-08 23:21:48 +0500

    Add Salom, dunyo! program

---
+++ lang/c-beginner/hello-world.c
@@ -0,0 +1,10 @@
+// Salom dunyo dasturi
+// Muallif: Xorazmiy Dev
+// Oxirgi o'zgartirilgan sana: 01/01/2023
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Salom, dunyo!\n");
+    return 0;
+}
