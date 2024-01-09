b0116992b57ba8b0e56ff6f02ed3786f7a6a0a84
Sitora Tuychieva <situychieva@gmail.com>
2024-01-09 19:08:32 +0500

    Add operator precedence

---
+++ lang/c-beginner/operator-precedence-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10, c = 15;
+    int result = a + b * c / 2;
+
+    printf("Natija: %d\n", result);
+    return 0;
+}
---
+++ lang/c-beginner/operator-precedence-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10, y = 20, z = 30;
+    int result = (x + y) * z; // Qavslar bajarilish tartibini o'zgartiradi
+
+    printf("Natija: %d\n", result);
+    return 0;
+}
