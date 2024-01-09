e360d865a5add0c051d63760490e94c3130aee97
Sitora Tuychieva <situychieva@gmail.com>
2024-01-09 16:27:50 +0500

    Add comments

---
+++ lang/c-beginner/comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  // Standart kirish-chiqish kutubxonasini qo'shish
+
+/* 
+   Dasturning bajarilishi shu funksiyadan boshlanadi.
+   Butun son qiymatini qaytaradi.
+*/
+int main() {
+    // Konsolga "Salom, dunyo!" deb chiqarish
+    printf("Hello, World!\n");
+
+    return 0;  // Dastur muvaffaqiyatli yakunlanganini bildiradi
+}
