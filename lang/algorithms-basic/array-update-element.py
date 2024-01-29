1f330a45534b91c51846edbc2cad5147c7118861
Sitora Tuychieva <situychieva@gmail.com>
2024-01-30 02:16:56 +0500

    update file comments

--- lang/algorithms-basic/array-update-element.py
+++ lang/algorithms-basic/array-update-element.py
@@ -1,4 +1,4 @@
 arr = [1, 2, 3]
-print(arr[2]) # chiquvchi ma'lumot: 3
+print(arr[2]) # Chiquvchi ma'lumot: 3
 arr[2] = 5
-print(arr[2]) # chiquvchi ma'lumot: 5
+print(arr[2]) # Chiquvchi ma'lumot: 5
