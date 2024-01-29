4d1e265e7d43bb27c7c7313ab8d8b1f8a608726a
Sitora Tuychieva <situychieva@gmail.com>
2024-01-30 00:44:08 +0500

    add linked-list.py

---
+++ lang/algorithms-basic/linked-list.py
@@ -0,0 +1,47 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        """ Yangi nod biriktirish """
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def find(self, key):
+        """ Qiymatiga ko’ra nodni topish """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
+    def print_list(self):
+        """ Ro’yxat elementlarini chop etish """
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+
+ll.print_list()  # Output: 1 -> 2 -> 3 -> None
+
+print("Node with data 2:", ll.find(2).data)  # Output: 2 elementli Node: 2
