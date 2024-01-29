82514fc92d0788949480431be9b92e14ced0186d
Sitora Tuychieva <situychieva@gmail.com>
2024-01-30 02:15:48 +0500

    update file comments

--- lang/algorithms-basic/bubble-sort.py
+++ lang/algorithms-basic/bubble-sort.py
@@ -11,7 +11,7 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-# Example usage
+# Foydalanish
 arr = [64, 34, 25, 12, 22, 11, 90]
 bubble_sort(arr)
 print("Sorted array is:")
