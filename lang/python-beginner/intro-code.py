8688f9dbcf3e8620752a76bcfe55aacf4b9467e3
Sitora Tuychieva <situychieva@gmail.com>
2024-01-30 22:24:49 +0500

    add intro python code

---
+++ lang/python-beginner/intro-code.py
@@ -0,0 +1,10 @@
+# Bu dasturning maqsadini tushuntiruvchi izoh
+
+# O'zgaruvchan deklaratsiya
+number = 5
+
+# Raqam manfiyligini tekshirish uchun shartli bayonot
+if number < 0:
+   print("Bu manfiy butun son.")
+else:
+   print("Bu musbat butun son.")
