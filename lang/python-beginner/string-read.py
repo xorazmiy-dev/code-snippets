3d9dbaab23bd9b06aa67f96929a169cd8e047789
Sitora Tuychieva <situychieva@gmail.com>
2024-02-01 16:26:05 +0500

    add string reading function

---
+++ lang/python-beginner/string-read.py
@@ -0,0 +1,6 @@
+birinchi_satr = "Python"
+
+# Indekslash yordamida alohida harflarni olish
+print(birinchi_satr[0]) # Output: P (0 indeksidagi belgi "P")
+print(birinchi_satr[1]) # Output: y
+print(birinchi_satr[-1]) # Output: n (birinchi_satr oxirgi elementi)
