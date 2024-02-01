273b4451566d7b350ee3c600d7ec0a094d308bf3
Sitora Tuychieva <situychieva@gmail.com>
2024-02-01 15:58:44 +0500

    add type conversion example

---
+++ lang/python-beginner/type-conversion.py
@@ -0,0 +1,19 @@
+# int dan floatga aniq konvertatsiya qilish
+
+int_number = 42
+float_number = float(int_number)
+print(float_number) # Output: 42.0
+
+# float dan int ga aniq konvertatsiya
+
+float_number = 3.14
+int_number = int(float_number)
+print(int_number) # Output: 3
+
+
+# int dan satrga aniq konvertatsiya
+
+int_number = 123
+str_value = int(int_number)
+
+print(str_value) # Output: '123'
