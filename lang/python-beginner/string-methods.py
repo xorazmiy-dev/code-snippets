84e1422c8903732bd6ad5df1fc3f5197a8f2bf93
Sitora Tuychieva <situychieva@gmail.com>
2024-02-01 17:10:01 +0500

    add string methods

---
+++ lang/python-beginner/string-methods.py
@@ -0,0 +1,6 @@
+original_string = "Salom"
+modified_string = original_string.upper() # upper() satr metod yordamida
+
+# Asl va o'zgartirilgan satrlarni chop etish
+print("Asl satr:", original_string)
+print("O’zgartirilgan satr:", modified_string)
