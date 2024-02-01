47161449624f4b2e8ca1887bb21a6c447b885761
Sitora Tuychieva <situychieva@gmail.com>
2024-02-01 15:46:58 +0500

    add none type example

---
+++ lang/python-beginner/none-type.py
@@ -0,0 +1,8 @@
+# None dan foydalanishni ko’rsatish uchun misol
+
+foydalanuvchi_yoshi = None
+print("Foydalanuvchi yoshi:", foydalanuvchi_yoshi) # Chiqish: Foydalanuvchi yoshi: None
+
+# Aytaylik, biz endi foydalanuvchining yoshini aniqladik
+foydalanuvchi_yoshi = 25
+print("Hozir foydalanuvchi yoshi:", foydalanuvchi_yoshi) # Chiqish: Foydalanuvchi yoshi: 25
