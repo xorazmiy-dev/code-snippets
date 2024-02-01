efa006e951477c9f9f3c6720a06d4c87a20dd300
Sitora Tuychieva <situychieva@gmail.com>
2024-02-01 16:29:06 +0500

    update code

--- lang/python-beginner/string-break.py
+++ lang/python-beginner/string-break.py
@@ -12,3 +12,9 @@ print(kesiladigan_satr[6:12])
 
 # Output: world (6 indeksdan boshlanadi, oxirida tugaydi)
 print(kesiladigan_satr[6:]) 
+
+# Output: Hello (0 indeksdan boshlanadi, oxirida bo'sh joy qoldirilgan 6 bilan tugaydi)
+print(kesiladigan_satr[:6]) 
+
+# Output: Hlow (0 indeksdan boshlanadi, 7da tugaydi, qadamlar har 2da)
+print(kesiladigan_satr[:7:2])
