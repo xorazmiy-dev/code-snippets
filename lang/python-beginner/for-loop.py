d2af339f18945987bbb0e0555e4f21bb51d7594c
Sitora Tuychieva <situychieva@gmail.com>
2024-02-05 20:36:26 +0500

    add for-loop.py

---
+++ lang/python-beginner/for-loop.py
@@ -0,0 +1,11 @@
+talabalar = 25
+
+
+for i in range(talabalar):
+    print(f'ID raqamiga ega talaba: {i}')
+
+# Chiqish:
+# ID raqamiga ega talaba: 0
+# ID raqamiga ega talaba: 1
+# ...
+# ID raqamiga ega talaba: 24
