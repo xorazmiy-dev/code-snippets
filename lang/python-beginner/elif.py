f68844904394442d42e0b0c8bfb8655849ba4be4
Sitora Tuychieva <situychieva@gmail.com>
2024-02-05 21:19:48 +0500

    add elif example

---
+++ lang/python-beginner/elif.py
@@ -0,0 +1,10 @@
+
+talaba_ismi = 'Bobur'
+gpa = 3.5
+
+if gpa >= 4.0:
+    print(f'{talaba_ismi}! Sizda juda baland GPA.')
+elif gpa >= 3.7:
+    print(f'Ajoyib, {talaba_ismi}! Siz katta mukofotga loyiqsiz.')
+else:
+    print(f'Davom eting, {talaba_ismi}. Sizning fidoyiligingiz o\'zini oqlaydi!')
