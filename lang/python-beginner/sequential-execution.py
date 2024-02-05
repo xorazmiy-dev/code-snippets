8dbe6a3aece8df5c570b954a1269f4aa7f82cc23
Sitora Tuychieva <situychieva@gmail.com>
2024-02-05 20:30:51 +0500

    add sequential execution

---
+++ lang/python-beginner/sequential-execution.py
@@ -0,0 +1,5 @@
+# Kod ketma-ket, birin-ketin bajariladi
+
+print('1-qadam: Bajarish boshlandi.')
+print('2-qadam: Bu yerda ba\'zi operatsiyalar bajariladi ...')
+print('3-qadam: Bajarish tugallandi.')
