a75982b3389ecc8943219e52766adaa46994a1f9
Sitora Tuychieva <situychieva@gmail.com>
2024-02-09 01:56:48 +0500

    add break statement example

---
+++ lang/python-beginner/break.py
@@ -0,0 +1,19 @@
+count = 1
+
+while count <= 5:
+   print(f"Joriy hisob: {count}")
+
+   if count == 3:
+       print("Tsikldan chiqib ketish.")
+       break
+
+   count += 1
+else:
+    print(f"Tsikl yakunlandi.")
+
+
+# Chiqish:
+# Joriy hisob: 1
+# Joriy hisob: 2
+# Joriy hisob: 3
+# Tsikldan chiqib ketish.
