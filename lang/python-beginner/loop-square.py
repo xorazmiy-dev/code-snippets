fb26c648348195d8a1f6af5e987e48d51274c3ba
Sitora Tuychieva <situychieva@gmail.com>
2024-02-09 00:52:47 +0500

    add for loop example with examples

---
+++ lang/python-beginner/loop-square.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+
+for number in numbers:
+    square = number ** 2
+    print(f'{number} sonining kvadrati {square} ga teng.') 
+
+
+# Chiqish:
+# 1 sonining kvadrati 1 ga teng.
+# 2 sonining kvadrati 4 ga teng.
+# 3 sonining kvadrati 9 ga teng.
