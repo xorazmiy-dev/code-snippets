88f9de5d5039c697baf3a237529fa083192482cb
Sitora Tuychieva <situychieva@gmail.com>
2024-02-09 01:01:58 +0500

    add for loop with indexing

---
+++ lang/python-beginner/loop-index.py
@@ -0,0 +1,10 @@
+talabalar = ['Alisher', 'Rustam', 'Abror']
+
+for index in range(len(talabalar)):
+    print(f'Indeks: {index}, Talaba: {talabalar[index]}')
+
+
+# Chiqish:
+# Indeks: 0, Talaba: Alisher
+# Indeks: 1, Talaba: Rustam
+# Indeks: 2, Talaba: Abror
