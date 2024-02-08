4086e4b9c0b4438826ad6f4e570af13d90548663
Sitora Tuychieva <situychieva@gmail.com>
2024-02-09 01:03:53 +0500

    add for loop with else statement

---
+++ lang/python-beginner/for-else.py
@@ -0,0 +1,11 @@
+numbers = [1, 2]
+
+for number in numbers:
+    print(f'Hozirgi son {number}') 
+else: 
+    print('Boshqa sonlar qolmadi.')
+
+# Chiqish:
+# Hozirgi son 1
+# Hozirgi son 2
+# Boshqa sonlar qolmadi.
