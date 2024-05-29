f9fddee80641806e41a1fd1bb78c9410e5be8e4b
Sitora Tuychieva <situychieva@gmail.com>
2024-05-29 18:54:51 +0500

    add test1.py file

---
+++ lang/python-beginner/test1.py
@@ -0,0 +1,17 @@
+count = 1
+
+while count <= 5:
+    if count % 2 == 0:
+       print(f"Juft raqamni o’tkazib yubordim: {count}")
+       count += 1
+       continue
+
+    print(f"Toq raqam: {count}")
+    count += 1
+
+# Chiqish:
+# Toq raqam: 1
+# Juft raqamni o’tkazib yubordim: 2
+# Toq raqam: 3
+# Juft raqamni o’tkazib yubordim: 4
+# Toq raqam: 5
