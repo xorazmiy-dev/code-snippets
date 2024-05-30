c6f1c245263bb79ed2447baaa1441d27792b5b01
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-05-30 13:11:03 -0700

    updated and added dict-type files

--- lang/python-beginner/dict-type.py
+++ lang/python-beginner/dict-type.py
@@ -1,17 +1,2 @@
 # Boshlang'ich qiymatlari bilan lug'at e’lon qilinish
-odam = {"ism": "Aziz", "yosh": 25, "vazn": 75.42} 
-
-# Alohida elementlarni olish
-
-print(odam["ism"]) # Output: Aziz
-print(odam["yosh"]) # Output: 25
-
-# Alohida elementlarni olish
-
-print(odam["ism"]) # Output: Aziz
-print(odam["yosh"]) # Output: 25
-
-# .get() usuli yordamida alohida elementlarni olish
-
-print(odam.get("vazn")) # Chiqish: 75.42
-print(odam.get("jins", 'Aniqlanmagan')) # Chiqish: Aniqlanmagan ("jins" kaliti lug'atimizda mavjud emas)
+odam = {"ism": "Aziz", "yosh": 25, "vazn": 75.42}
