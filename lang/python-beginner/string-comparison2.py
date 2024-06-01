7c8d75489b4231a6ce20e4d9973fd3abde5da675
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-05-31 22:31:26 -0700

    typo correct

--- lang/python-beginner/string-comparison2.py
+++ lang/python-beginner/string-comparison2.py
@@ -2,6 +2,6 @@ str1 = "salom"
 str2 = "satr"
 
 if str1 < str2:
-    print(f"{str1} leksikografik tartibda {str2} dan olding keladi.")
+    print(f"{str1} leksikografik tartibda {str2} dan oldin keladi.")
 else:
     print(f"{str2} leksikografik tartibda {str1} dan oldin keladi.")
