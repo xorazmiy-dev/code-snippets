a765c27850e4278024174d66c022f31eca1859af
Sitora Tuychieva <92748689+situychieva@users.noreply.github.com>
2024-06-01 18:06:33 +0500

    Update round.rb

--- lang/ruby/round.rb
+++ lang/ruby/round.rb
@@ -1,3 +1,6 @@
-puts 1.pred # 0 soni chop etiladi
-puts 99.pred # 98 soni chop etiladi
-puts -3.pred # -4 soni chop etiladi
+puts 1.4.round # 1 raqamini chop etadi
+puts 1.5.round # 2 raqamini chop etadi
+puts 1.8.round # 2 raqamini chop etadi
+puts 1.14645.round(2) # 1.15 sonini chop etadi
+puts 1.15645.round(2) # 1.16 sonini chop etadi
+puts 1.15645.round(2) # 7.16 sonini chop etadi
