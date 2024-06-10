56462037b683fe2a316da9ad5eaaf8750b26a3e1
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-10 00:00:20 -0700

    typo correct

--- lang/python-beginner/nested-if.py
+++ lang/python-beginner/nested-if.py
@@ -2,4 +2,4 @@ talaba_ismi = 'Bobur'
 gpa = 3.8
 
 if gpa >= 3.5:
-    print(f'Bundan tashqari, {talaba_ismi}, siz erishgan yutuqlaringizga asoslangan qo'shimcha stipendiya olish huquqiga egasiz!')
+    print(f'Bundan tashqari, {talaba_ismi}, siz erishgan yutuqlaringizga asoslangan qo\'shimcha stipendiya olish huquqiga egasiz!')
