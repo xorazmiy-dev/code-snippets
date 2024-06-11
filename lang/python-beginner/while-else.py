dd83c84716d64f8c0582d083122ece81ce67ecb7
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-11 01:15:50 -0700

    updates

--- lang/python-beginner/while-else.py
+++ lang/python-beginner/while-else.py
@@ -4,10 +4,10 @@ while start <= 3:
    print(count)
    count += 1
 else:
-   print("Tsikl muvaffaqiyatli yakunlandi.")
+   print("Halqa muvaffaqiyatli yakunlandi.")
 
-# Chiqish:
+# Natija:
 # 1
 # 2
 # 3
-# Tsikl muvaffaqiyatli yakunlandi.
+# Halqa muvaffaqiyatli yakunlandi.
