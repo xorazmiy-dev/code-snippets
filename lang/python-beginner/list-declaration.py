cdb92241a13e119656f569117f5bfd50ea2c2e34
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-13 13:40:36 -0700

    add new file

---
+++ lang/python-beginner/list-declaration.py
@@ -0,0 +1,14 @@
+# Bo’sh ro’yxatni yaratish
+empty_list = []
+
+# Butun sonlar ro’yxatini yaratish
+numbers = [1, 2, 3]
+
+# Satrlar ro’yxatini yaratish
+students = ['Alisher', 'Abror']
+
+# Aralash turdagi ro’yxat yaratish
+mixed_list = ['Aziz', 30, True, 3.14]
+
+# Ro’yxat ichida ro’yxat yaratish
+nested_list = [[1, 2, 3], students]
