dcbdef1c4c0a144d97f4b866c6ea5b70d069f669
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-13 13:46:59 -0700

    add list indexes

---
+++ lang/python-beginner/list-indexes.py
@@ -0,0 +1,8 @@
+# Butun sonlar ro’yxatini yaratish
+numbers = [1, 2, 3]
+
+# Birinchi elementi olish
+first_number = numbers[0] # Natija: 1
+
+# Oxirgi elementni olish
+last_mumber = numbers[-1] # Natija: 3
