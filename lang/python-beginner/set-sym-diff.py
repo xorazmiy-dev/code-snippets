7b19945314cd6efaa3e1c87f2e3602fd55f8462d
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-20 14:18:02 -0700

    file update

--- lang/python-beginner/set-sym-diff.py
+++ lang/python-beginner/set-sym-diff.py
@@ -2,6 +2,6 @@
 set1 = {1, 2, 3}
 set2 = {3, 4, 5}
 
-symmetric_diff = set1.symetric_difference(set2)
+symmetric_diff = set1.symmetric_difference(set2)
 print(symmetric_diff)
 # Natija: {1, 2, 4, 5}
