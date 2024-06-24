eae2c67a7aac613c49e5564b7813b75170b31c8e
LAPTOP-D4GT9EBK\Nozima <nozima@appsolutemedia.com>
2024-06-24 15:14:32 -0700

    file update

--- lang/python-beginner/dict-get.py
+++ lang/python-beginner/dict-get.py
@@ -2,7 +2,7 @@
 
 talabalar = {'ism': 'Al-Xorazmiy', 'baholar': [89, 90], 'yosh': 20}
 yosh_qiymati = talabalar.get('yosh', 0) # standart qiymat belgilangan: 0
-manzil = students.get('manzil', 'Mavjud emas') # standart qiymat
+manzil = talabalar.get('manzil', 'Mavjud emas') # standart qiymat
 
 print(yosh_qiymati)
 print(manzil)
