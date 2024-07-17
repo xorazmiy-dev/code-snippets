2993e7329443cd3fab3c9d2f7df6a24b9dbd6317
Ulfat <ulfat.abjabborov@gmail.com>
2024-07-17 17:17:34 +0200

    Update and rename operators.c to arithmetic-operators.c

---
+++ lang/c/arithmetic-operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 2 + 3;
+    int sub = 2 - 3;
+    int mul = 2 * 3;
+    int div = 4 / 2;
+    int mod = 2 % 3;
+
+    printf("sum = %d\n", sum);
+    printf("sub = %d\n", sub);
+    printf("mul = %d\n", mul);
+    printf("div = %d\n", div);
+    printf("mod = %d\n", mod);
+
+    return 0;
+}
