0bce3664592e13bbf3bad87169b63161af1b3ec7
Ulfat <ulfat.abjabborov@gmail.com>
2024-07-29 07:52:50 +0200

    Update symbolic-constants-1.c

--- lang/c-beginner/symbolic-constants-1.c
+++ lang/c-beginner/symbolic-constants-1.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    const int son = 10; // O'zgarmas butun sonni e'lon qilish
-    printf("son ning asl qiymati: %d\n", son);
-
-    // son ning qiymatini o'zgartirishga urinish
-    son = 20; // Bu qator kompilyatsiya vaqtida xatoga sabab bo'ladi
-
-    printf("son ning yangi qiymati: %d\n", son);
+    const int son = 10;
+    son = 20;
+    
     return 0;
 }
