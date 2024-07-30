731179ca4e58465e8ea1d5d6640e9b91729c4d75
Ulfat <ulfat.abjabborov@gmail.com>
2024-07-30 08:02:11 +0200

    Update assignment-operators-1.c

--- lang/c-beginner/assignment-operators-1.c
+++ lang/c-beginner/assignment-operators-1.c
@@ -2,10 +2,9 @@
 
 int main() {
     int x = 10;
-
-    x = x + 5; // x ga 5 qo'shadi va keyin natijani yana x ga tayinlaydi.
-
-    printf("Qo'shish bilan o'z-o'ziga tayinlangandan keyin x ning qiymati: %d\n", x);
+    x = x + 5;
+    
+    printf("x ning qiymati: %d\n", x);
 
     return 0;
 }
