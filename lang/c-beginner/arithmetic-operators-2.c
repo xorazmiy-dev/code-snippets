c15931c74c35f13c00e9ea743155f1f43deec6a0
Ulfat <ulfat.abjabborov@gmail.com>
2024-07-30 08:25:12 +0200

    Update arithmetic-operators-2.c

--- lang/c-beginner/arithmetic-operators-2.c
+++ lang/c-beginner/arithmetic-operators-2.c
@@ -4,10 +4,11 @@ int main() {
     int x = 10;
     int y = 10;
 
-    x++; // x ga 1 qo'shadi va keyin natijani yana x ga tayinlaydi.
-    y--; // y ga 1 qo'shadi va keyin natijani yana y ga tayinlaydi.
+    x++;
+    y--;
 
-    printf("Oshirish operatoridan keyingi x ning qiymati: %d\n", x);
-    printf("Kamaytirish operatoridan keyingi y ning qiymati: %d\n", y);
+    printf("x ning qiymati: %d\n", x);
+    printf("y ning qiymati: %d\n", y);
+    
     return 0;
 }
