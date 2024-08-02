f0adcaf3ddd7e15ed3a6e8c3bcdf665f1d495839
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-02 08:30:39 +0200

    Update logical-operators-1.c

--- lang/c-beginner/logical-operators-1.c
+++ lang/c-beginner/logical-operators-1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a = 1;  // Nol bo'lmagan qiymat rost deb baholanadi
-    int b = 0;  // yolg'on deb baholanadi
+    int a = 1;  // true
+    int b = 0;  // false
 
-    int natija = a && b; // Mantiqiy AND
+    int natija = a && b; // AND
 
     printf("natija: %d\n", natija);
     return 0;
