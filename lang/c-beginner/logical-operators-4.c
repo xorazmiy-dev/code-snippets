3ce3f6c61087338402d573aac4e9b2acad78b159
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-02 08:31:59 +0200

    Update logical-operators-4.c

--- lang/c-beginner/logical-operators-4.c
+++ lang/c-beginner/logical-operators-4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a = 1; // rost
-    int b = 0; // yolg'on
-    int c = 1; // rost
+    int a = 1; // true
+    int b = 0; // false
+    int c = 1; // true
 
     // Murakkab mantiqiy ifoda
     int natija = (a && !b) || (c && !a);
 
-    printf("Natija: %d\n", natija); // natijani chop etadi
+    printf("Natija: %d\n", natija);
 
     return 0;
 }
