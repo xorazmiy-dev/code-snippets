53578b30aa1d257998164a49c3065460bd42e416
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-06 07:11:52 +0200

    Update relatiional-operators-ne.c

--- lang/c-beginner/relatiional-operators-ne.c
+++ lang/c-beginner/relatiional-operators-ne.c
@@ -4,8 +4,8 @@ int main() {
     int a = 2;
     int b = 3;
     
-    int natija = a == b;
-    printf("natija: %d", natija)
+    int natija = a != b;
+    printf("natija: %d", natija);
 		
-    return 0
+    return 0;
 }
