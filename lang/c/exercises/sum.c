7166f4589d42bc3157acdb58b094832f3feca359
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-16 08:09:53 +0200

    Create sum.c

---
+++ lang/c/exercises/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // kodni shu yerga yozing
+  int a = 10;
+  int b = 20;
+  int c = 30;
+
+  int sum = a + b + c;
+
+  printf("sum: %d\n", sum);
+  
+  return 0;
+}
