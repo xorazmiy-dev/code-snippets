ee78f63166557e0daa1f7b38eb64a205fed99038
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-19 21:01:12 +0200

    Create area.c

---
+++ lang/c/exercises/area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // kodni shu yerga yozing
+  float a = 10;
+  float b = 20;
+
+  // to'rtburchakning yuzasi uning tomonlari ko'paytmasiga teng.
+  float p = a * b; 
+
+  printf("Javob: %f\n", p);
+  
+  return 0;
+}
