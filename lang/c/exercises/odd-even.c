46112934c878d11af2920bc4da51e05a9233c2c5
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-20 07:01:05 +0200

    Create odd-even.c

---
+++ lang/c/exercises/odd-even.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+  // kodni shu yerga yozing
+  int a = 10;
+  int b = 20;
+ 
+  int c = a + b;
+  
+  int mod = c % 2;
+
+  if (mod == 0) {
+    printf("juft");
+  } else {
+    printf("toq");
+  }
+  
+  return 0;
+}
