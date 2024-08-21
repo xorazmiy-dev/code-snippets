253fc93c0911d09e7018459162b7ee9c878fbaec
Ulfat <ulfat.abjabborov@gmail.com>
2024-08-21 08:03:17 +0200

    Create square-func.c

---
+++ lang/c/exercises/square-func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int square(int a) {
+  return a * a;
+}
+
+int main() {
+  // kodni shu yerga yozing
+  int a = 10;
+
+  int natija = square(a);
+  
+  printf("Natija: %d\n", natija);
+  
+  return 0;
+}
