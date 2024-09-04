232b7ef9d7872766398f7630b532b792ec2b668c
Ulfat <ulfat.abjabborov@uber.com>
2024-09-04 22:03:51 +0200

    Create scanf-float.c

---
+++ lang/c-beginner/exercises/scanf-float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float a; // o'zgaruvchi yaratiladi
+
+    scanf("%f", &a); // konsoldagi qiymat o'zgaruvchiga saqlanadi
+
+    printf("%f\n", a); // o'zgaruvchining qiymati konsolga chiqariladi
+
+    return 0;
+}
