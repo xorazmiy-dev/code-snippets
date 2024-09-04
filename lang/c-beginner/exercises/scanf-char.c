e7499b120166bb94c3f9ff3291d1ec79ff5ef3ac
Ulfat <ulfat.abjabborov@uber.com>
2024-09-04 22:07:35 +0200

    Create scanf-char.c

---
+++ lang/c-beginner/exercises/scanf-char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char c; // o'zgaruvchi yaratiladi
+
+    scanf("%c", &c); // konsoldagi qiymat o'zgaruvchiga saqlanadi
+
+    printf("%c\n", c); // o'zgaruvchining qiymati konsolga chiqariladi
+
+    return 0;
+}
