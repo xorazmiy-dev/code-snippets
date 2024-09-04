1438311b1a36c7e2c65a96b287e0507108f9a2e2
Ulfat <ulfat.abjabborov@uber.com>
2024-09-04 22:16:15 +0200

    Create scanf-int-2.c

---
+++ lang/c-beginner/exercises/scanf-int-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a; // variable a
+    int b; // variable b
+
+    scanf("%d", &a); // 1-kirish qiymatini a ga saqlash
+    scanf("%d", &b); // 2-kirish qiymatini b ga saqlash
+
+    printf("%d\n", a); // a ning qiymatini konsolga chiqarish
+    printf("%d\n", b); // b ning qiymatini konsolga chiqarish
+
+    return 0;
+}
