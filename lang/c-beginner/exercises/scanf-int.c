2a31e4c057cc90f7a4380c1c1f896093a8750d8f
Ulfat <ulfat.abjabborov@uber.com>
2024-09-04 22:01:40 +0200

    Create scanf-int.c

---
+++ lang/c-beginner/exercises/scanf-int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a; // o'zgaruvchi yaratiladi
+
+    scanf("%d", &a); // konsoldagi qiymat o'zgaruvchiga saqlanadi
+
+    printf("%d\n", a); // o'zgaruvchining qiymati konsolga chiqariladi
+
+    return 0;
+}
