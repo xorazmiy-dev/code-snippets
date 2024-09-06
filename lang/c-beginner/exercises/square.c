8426fd78d9336eff49e54491462cf93304b1e070
Ulfat <ulfat.abjabborov@uber.com>
2024-09-06 08:13:29 +0200

    Create square.c

---
+++ lang/c-beginner/exercises/square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a; // variable a
+
+    scanf("%d", &a); // kirish qiymatini variable ga saqlash
+
+    a = a * a; // a ning kvadrati 
+
+    printf("%d\n", a);
+
+    return 0;
+}
