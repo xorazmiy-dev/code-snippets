f3175a52f7b72304f67244ea5ffdf6fdda24f98a
MukhlisaMirajiboyeva <mirtajiboyeva_mukhlisa@gmail.com>
2024-09-07 16:47:38 +0500

    add ruby code

---
+++ lang/ruby/l-6ran.rb
@@ -0,0 +1,24 @@
+# sonli ketma-ketlik hosil qilish
+sonli_range = 1..100
+
+
+# sonli ketma-ketlikni chop etish
+puts sonli_range
+
+
+# sonli_range o'zgaruvchisining ma'lumot turini chop etish
+puts sonli_range.class # Range
+
+
+
+
+# harfli ketma-ketlik hosil qilish
+harfli_range = 'c'..'m'
+
+
+# harfli ketma-ketlikni chop etish
+puts harfli_range
+
+
+# harfli_range o'zgaruvchisining ma'lumot turini chop etish
+puts harfli_range.class # Range
---
+++ lang/ruby/l-6ran2.rb
@@ -0,0 +1,14 @@
+# 101 dan 200 gacha bo'lgan sonlarni qamrovchi massiv
+sonli_massiv = (101..200).to_a
+
+
+# "sonli_massiv"ni chop etish
+pp sonli_massiv
+
+
+# 'a' dan 'z' gacha bo'lgan harflarni qamrovchi massiv
+harfli_massiv = ('a'..'z').to_a
+
+
+# "harfli_massiv"ni chop etish
+puts harfli_massiv
