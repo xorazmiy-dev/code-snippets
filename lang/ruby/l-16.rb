f085523e21fd141a1462995e77aef83f82e5d682
MukhlisaMirajiboyeva <mirtajiboyeva_mukhlisa@gmail.com>
2024-09-07 17:10:12 +0500

    add L16 code

---
+++ lang/ruby/l-16.rb
@@ -0,0 +1,15 @@
+baho = 40
+
+
+case baho
+when 90..100
+  puts "A'lo baho"
+when 80..89
+  puts "Yaxshi baho"
+when 70..79
+  puts "Qoniqarli baho"
+else
+  puts "Qoniqarsiz baho"
+end
+# Dastur chop etadi:
+# Qoniqarsiz baho
