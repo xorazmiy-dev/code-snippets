1c24e6235ccb3781a3a6348561b91e0ace08e3ce
Ulfat <ulfat.abjabborov@gmail.com>
2024-09-09 07:13:08 +0200

    Add circle-area.c

---
+++ lang/c/exercises/circle-area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  // kodni shu yerga yozing
+  float R;
+  const pi = 3.14;
+
+  scanf("%f", &R);
+
+  float S = pi * R * R;
+
+  printf("%f\n", S);
+  
+  return 0;
+}
