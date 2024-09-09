5165d520cfcec1bdc8178b46d79943867f9f9000
Ulfat <ulfat.abjabborov@gmail.com>
2024-09-09 06:57:20 +0200

    feat: Add complex arithmetic program in C

---
+++ lang/c/exercises/complex-arith.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // kodni shu yerga yozing
+
+  float a = 10.9;
+  float b = 5.5;
+
+  float natija = a*a + 2*a*b + b*b;
+
+  printf("%f\n", natija);
+  
+  return 0;
+}
